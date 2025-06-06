4c3edaca369cae67bfea5bfeda1ff4d2988be859
Sabarish V S <Sabarish_7@protonmail.com>
2025-06-06 08:33:53 +0530

    Added env and dependancies [tanstackuery, Motion/react]

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
+if (import.meta.env.VITE_MODE === "development") {
+  const script = document.createElement("script");
+  script.src = "https://unpkg.com/react-scan/dist/auto.global.js";
+  script.async = true;
+  document.head.appendChild(script);
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
