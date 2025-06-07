70137b4dc1b994484f09af99a1ba04098009648e
Sabarish V S <Sabarish_7@protonmail.com>
2025-06-07 15:06:22 +0530

    added suspense animation

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
-import MainLayout from "./components/layout/MainLayout";
+import "./index.css";
+
 if (import.meta.env.VITE_MODE === "development") {
   const script = document.createElement("script");
   script.src = "https://unpkg.com/react-scan/dist/auto.global.js";
@@ -12,8 +13,8 @@ if (import.meta.env.VITE_MODE === "development") {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MainLayout>
+    <BrowserRouter>
       <App />
-    </MainLayout>
+    </BrowserRouter>
   </StrictMode>
 );
